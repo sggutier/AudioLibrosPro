eba0b5fe3ae9bf679d6924f02facad2a28e1dba4
Saul German Gutierrez Calderon <me@sggc.me>
2020-02-03 22:11:23 -0600

    Añadida gestion chafa de fragmentos

---
+++ app/src/main/java/itsur/german/audiolibrospro/ServicioReproduccion.java
@@ -0,0 +1,56 @@
+package itsur.german.audiolibrospro;
+
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class ServicioReproduccion extends Service {
+    private MediaPlayer mediaPlayer;
+    private final BinderReproduccion binder = new BinderReproduccion();
+
+    public class BinderReproduccion extends Binder {
+        public ServicioReproduccion obtenServicio() {
+            return ServicioReproduccion.this;
+        }
+    }
+
+    public MediaPlayer getMediaPlayer() {
+        return mediaPlayer;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d("aoeu", "Servicio creado");
+        mediaPlayer = new MediaPlayer();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (!mediaPlayer.isPlaying()) {
+            mediaPlayer.start();
+        }
+        return START_STICKY;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+        }
+        mediaPlayer.release();
+    }
+}
--- app/src/main/java/itsur/german/audiolibrospro/fragments/DetalleFragment.java
+++ app/src/main/java/itsur/german/audiolibrospro/fragments/DetalleFragment.java
@@ -1,11 +1,17 @@
 package itsur.german.audiolibrospro.fragments;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +19,7 @@ import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceManager;
 
@@ -22,9 +29,12 @@ import itsur.german.audiolibrospro.Aplicacion;
 import itsur.german.audiolibrospro.Libro;
 import itsur.german.audiolibrospro.MainActivity;
 import itsur.german.audiolibrospro.R;
+import itsur.german.audiolibrospro.ServicioReproduccion;
+import itsur.german.audiolibrospro.ServicioReproduccion.BinderReproduccion;
 
 public class DetalleFragment extends Fragment implements View.OnTouchListener, MediaPlayer.OnPreparedListener, MediaController.MediaPlayerControl {
     public static String ARG_ID_LIBRO = "id_libro";
+    ServicioReproduccion mServicioReproduccion;
     MediaPlayer mediaPlayer;
     MediaController mediaController;
 
@@ -49,6 +59,7 @@ public class DetalleFragment extends Fragment implements View.OnTouchListener, M
         View vista = inflador.inflate(R.layout.fragment_detalle,
                 contenedor, false);
         Bundle args = getArguments();
+
         if (args != null) {
             int position = args.getInt(ARG_ID_LIBRO);
             ponInfoLibro(position, vista);
@@ -59,25 +70,45 @@ public class DetalleFragment extends Fragment implements View.OnTouchListener, M
     }
 
     private void ponInfoLibro(int id, View vista) {
-        Libro libro = ((Aplicacion) getActivity().getApplication()).getVectorLibros().elementAt(id);
+        final Libro libro = ((Aplicacion) getActivity().getApplication()).getVectorLibros().elementAt(id);
         ((TextView) vista.findViewById(R.id.titulo)).setText(libro.titulo);
         ((TextView) vista.findViewById(R.id.autor)).setText(libro.autor);
-        ((ImageView) vista.findViewById(R.id.portada))
-                .setImageResource(libro.recursoImagen);
+        ((ImageView) vista.findViewById(R.id.portada)).setImageResource(libro.recursoImagen);
         vista.setOnTouchListener(this);
-        if (mediaPlayer != null){
-            mediaPlayer.release();
-        }
-        mediaPlayer = new MediaPlayer();
-        mediaPlayer.setOnPreparedListener(this);
-        mediaController = new MediaController(getActivity());
-        Uri audio = Uri.parse(libro.urlAudio);
-        try {
-            mediaPlayer.setDataSource(getActivity(), audio);
-            mediaPlayer.prepareAsync();
-        } catch (IOException e) {
-            Log.e("Audiolibros", "ERROR: No se puede reproducir "+audio,e);
-        }
+//        if (mediaPlayer != null){
+//            mediaPlayer.release();
+//        }
+//        mediaPlayer = new MediaPlayer();
+
+        ServiceConnection gestionConexion = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                Log.d("woof", "Servicio conectadezco");
+                BinderReproduccion binder = (BinderReproduccion) service;
+                mServicioReproduccion = binder.obtenServicio();
+                Log.d("aosnth", "Se conecto al servicio");
+
+                mediaPlayer = mServicioReproduccion.getMediaPlayer();
+                mediaPlayer.setOnPreparedListener(DetalleFragment.this);
+                mediaController = new MediaController(getActivity());
+                Uri audio = Uri.parse(libro.urlAudio);
+                try {
+                    mediaPlayer.setDataSource(getActivity(), audio);
+                    mediaPlayer.prepareAsync();
+                } catch (IOException e) {
+                    Log.e("Audiolibros", "ERROR: No se puede reproducir "+audio,e);
+                }
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+                Log.d("aosnth", "Se desconecto del servicio");
+            }
+        };
+        Context contexto = this.getContext();
+        Intent intent = new Intent(this.getContext(), ServicioReproduccion.class);
+        contexto.bindService(intent, gestionConexion, Context.BIND_AUTO_CREATE);
+
     }
 
     @Override
